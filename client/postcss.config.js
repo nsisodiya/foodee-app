917f5a86ad25f2f0f201f424a3a790023edc1c68
Narendra Sisodiya <narendra.sisodiya.1983@gmail.com>
2020-08-12 14:29:24 +0530

    purgecss working fine

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,5 +1,13 @@
 const tailwindcss = require('tailwindcss');
+const purgeCSS = require('@fullhuman/postcss-purgecss');
 
 module.exports = {
-  plugins: [tailwindcss('./src/tailwind.config.js'), require('autoprefixer')]
+  plugins: [
+    tailwindcss('./src/tailwind.config.js'),
+    purgeCSS({
+      content: ['./src/**/*.js'],
+      css: ['./src/**/*.css']
+    }),
+    require('autoprefixer')
+  ]
 };
